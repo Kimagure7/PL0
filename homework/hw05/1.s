be8793f7ddc2b32ac93a7854c0bd962954d42e9c
Kimagure <re2477036@mail.ustc.edu.cn>
2022-11-06 16:15:26 +0800

    gcc by linux

---
+++ homework/hw05/1.s
@@ -0,0 +1,64 @@
+	.file	"1.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%x %s\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movb	$50, -16(%rbp)
+	movb	$48, -15(%rbp)
+	movb	$49, -14(%rbp)
+	movb	$54, -13(%rbp)
+	movb	$0, -12(%rbp)
+	leaq	-16(%rbp), %rax
+	movq	%rax, -24(%rbp)
+	movl	-16(%rbp), %eax
+	movq	-24(%rbp), %rdx
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L3
+	call	__stack_chk_fail@PLT
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
