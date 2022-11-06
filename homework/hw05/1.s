8d7a9c1ad8ee7c12df72eca2ebf96b7964e9044e
Kimagure <re2477036@mail.ustc.edu.cn>
2022-11-06 18:49:03 +0800

    1

--- homework/hw05/1.s
+++ homework/hw05/1.s
@@ -1,42 +1,95 @@
 	.file	"1.c"
 	.text
-	.def	__main;	.scl	2;	.type	32;	.endef
-	.section .rdata,"dr"
+	.section	.rodata
 .LC0:
-	.ascii "%x %s\12\0"
+	.string	"%x %s\n"
 	.text
 	.globl	main
-	.def	main;	.scl	2;	.type	32;	.endef
-	.seh_proc	main
+	.type	main, @function
 main:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$48, %rsp
-	.seh_stackalloc	48
-	.seh_endprologue
-	call	__main
-	movb	$50, -16(%rbp)
-	movb	$48, -15(%rbp)
-	movb	$49, -14(%rbp)
-	movb	$54, -13(%rbp)
-	movb	$0, -12(%rbp)
-	# 上面初始化2016\0
-	leaq	-16(%rbp), %rax #取出rbp的值 -16 赋给%rax
-	#对应c = (char *)&data;
-	movq	%rax, -8(%rbp) #一个指针64位
-	movl	-16(%rbp), %eax #movl一个字节 一个int4个字节
-	movq	-8(%rbp), %rdx
-	movq	%rdx, %r8
-	movl	%eax, %edx
-	leaq	.LC0(%rip), %rcx
-	call	printf
-	#以下为退出复原
+.LFB0:
+	.cfi_startproc
+	endbr32
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	movl	%esp, %ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	pushl	%ebx
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x78,0x6
+	.cfi_escape 0x10,0x3,0x2,0x75,0x7c
+	subl	$16, %esp
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	movl	%gs:20, %ecx
+	movl	%ecx, -12(%ebp)
+	xorl	%ecx, %ecx
+	movb	$50, -20(%ebp)
+	movb	$48, -19(%ebp)
+	movb	$49, -18(%ebp)
+	movb	$54, -17(%ebp)
+	movb	$0, -16(%ebp)
+	leal	-20(%ebp), %edx
+	movl	%edx, -24(%ebp)
+	movl	-20(%ebp), %edx
+	subl	$4, %esp
+	pushl	-24(%ebp)
+	pushl	%edx
+	leal	.LC0@GOTOFF(%eax), %edx
+	pushl	%edx
+	movl	%eax, %ebx
+	call	printf@PLT
+	addl	$16, %esp
 	movl	$0, %eax
-	addq	$48, %rsp
-	popq	%rbp
+	movl	-12(%ebp), %ecx
+	xorl	%gs:20, %ecx
+	je	.L3
+	call	__stack_chk_fail_local
+.L3:
+	leal	-8(%ebp), %esp
+	popl	%ecx
+	.cfi_restore 1
+	.cfi_def_cfa 1, 0
+	popl	%ebx
+	.cfi_restore 3
+	popl	%ebp
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
 	ret
-	.seh_endproc
-	.ident	"GCC: (x86_64-win32-seh-rev0, Built by MinGW-W64 project) 8.1.0"
-	.def	printf;	.scl	2;	.type	32;	.endef
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
+	.globl	__x86.get_pc_thunk.ax
+	.hidden	__x86.get_pc_thunk.ax
+	.type	__x86.get_pc_thunk.ax, @function
+__x86.get_pc_thunk.ax:
+.LFB1:
+	.cfi_startproc
+	movl	(%esp), %eax
+	ret
+	.cfi_endproc
+.LFE1:
+	.hidden	__stack_chk_fail_local
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 4
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 4
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 4
+4:
